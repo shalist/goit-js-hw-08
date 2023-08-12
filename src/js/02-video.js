f4e31bf020ae994e3c1bd00e8a3e3526218c64d6
shalist <NE401960@gmail.com>
2023-08-12 20:11:47 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import vimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+// Отримуємо ключ для збереження у локальному сховищі
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+// Відстежуємо подію оновлення часу відтворення
+player.on('timeupdate', throttle(function (data) {
+    const currentTime = data.seconds; // Отримуємо поточний час відтворення
+    saveCurrentTime(currentTime);
+}, 1000));
+
+// Функція для збереження поточного часу відтворення у локальному сховищі
+function saveCurrentTime(currentTime) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, currentTime);
+}
+
+// Отримуємо поточний час відтворення і зберігаємо його у локальному сховищі
+player.getCurrentTime().then(function (currentTime) {
+    saveCurrentTime(currentTime);
+});
+
+// Отримуємо заголовок відео
+player.getVideoTitle().then(function (title) {
+    console.log('Video title:', title);
+});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
+// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми.В іншому випадку поля повинні бути порожніми.
+// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд.Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
+
+import throttle from "lodash.throttle";
+
+const elForm = document.querySelector(".feedback-form");
+elForm.addEventListener('input', throttle(handlerFeedbackForm), 500);
+
+
+const inputValue = "feedback-form-state";
+
+function handlerFeedbackForm(evt) {
+    evt.preventDefault();
+    // console.log(evt.currentTarget.elements);
+    const { email, message } = evt.currentTarget.elements
+    // console.log("Email", 
+    // console.log("Message", message.value);
+};
 
